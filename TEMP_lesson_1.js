5b8a5eb142095f70adb2f397a96f8fe4ecb4c559
karlchvojka <karl.chvojka@gmail.com>
2024-08-03 06:21:53 -0400

    DOC: Adding of Starting docs
    Adding of Readme documentation, and Lesson template file

---
+++ TEMP_lesson_1.js
@@ -0,0 +1,13 @@
+/**
+ * Lesson Title <Lesson #>
+ * @description Quick Description <Lesson Number Here>
+ * @see {@link <Lesson URL Here>}
+ * 
+ * @param {string} none - No params needed
+ * @example 
+ * const F functionName()
+ * @returns {string} <Lessong Goal>
+ */
+export default ComponentExample = () => {
+  return x
+}
